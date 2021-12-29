28a6c95f9f07c4fc36bd09b8ca9ca7cb9209218d
max.ottenhof@gmail.com <max.ottenhof@gmail.com>
2021-12-29 23:51:13 +0100

    psot 119 forms

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, OnInit, Output, ViewChild, EventEmitter} from '@angular/core';
 import {Ingredient} from "../../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list-service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,8 +10,7 @@ import {ShoppingListService} from "../shopping-list-service";
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('nameInput')nameInputRef: ElementRef;
-  @ViewChild('amountInput')amountInputRef: ElementRef;
+
   // @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor(private shoppingListService: ShoppingListService) { }
@@ -18,10 +18,9 @@ export class ShoppingEditComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(form: NgForm) {
+    const value = form.value
+    const newIngredient = new Ingredient(value.name, value.amount);
     // this.ingredientAdded.emit(newIngredient);
       // this.ingredientAdded.emit(newIngredient);
       // this.shoppingListService.shoppingListAdded.emit(newIngredient);
