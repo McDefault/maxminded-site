234bf1d260084ff708fc3589a2798918c0d12bff
max.ottenhof@gmail.com <max.ottenhof@gmail.com>
2022-01-17 16:15:54 +0100

    post 301 UI authentication

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,23 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {DataStorageService} from "../shared/data-storage.service";
+import {AuthService} from "../auth/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private subscriptionUser: Subscription;
   collapsed = true;
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService) { }
   ngOnInit(): void {
+    this.subscriptionUser = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    })
   }
 
   onClickSaveData() {
@@ -19,5 +26,9 @@ export class HeaderComponent implements OnInit {
 
   onCLickFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionUser.unsubscribe();
   }
 }
