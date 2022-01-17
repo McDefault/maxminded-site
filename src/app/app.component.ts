1f1ae4dc6191fd658f964b3e81a5e7f9060bca4d
max.ottenhof@gmail.com <max.ottenhof@gmail.com>
2022-01-17 18:27:47 +0100

    post 305 store user in localstorage to keep session persistant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import {Component, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./auth/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'maxminded-site';
   loadedFeature = 'recipe';
   disabled;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authService.autoLogin();
+  }
+
 
 }
